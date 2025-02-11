f4e856e7b58445bf351ad26f007b65091f256645
Pedro Talalayv <pedrotalalayv6767@gmail.com>
2025-02-11 16:34:29 -0300

    seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,13 +1,20 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[]args) {
 		
 	
-Department obj = new Department(1, "books");
-	System.out.println(obj);
+	Department obj = new Department(1, "books");
+
+	Seller seller = new Seller(21, "Janaina","jahpecampos@gmail.com", new Date(), 4000.0, obj);
+	
+	System.out.println(seller);
+	
 	}
 
 }
